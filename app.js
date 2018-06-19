31cf1b98ce83d63026d873598d11d30b15579498
Sooraj K S <sooraj007ks@gmail.com>
2018-06-19 06:51:07 +0530

    index cofig

--- app.js
+++ app.js
@@ -28,14 +28,16 @@ app.use('/', authRouter);
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     app.get('*', (req, res)=> {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile('/client/build/index.html');
+
     });
 }
 
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: true
+// }));
 
 // app.use('/', indexRouter);
 
